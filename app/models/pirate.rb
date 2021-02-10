c1be91d5b22f12d8e0d171aa92d92fd5ae574e00
NR598 <norm.rich@gmail.com>
2021-02-10 16:35:31 -0500

    commit tree 0c066ce9d0e652dc55b3e4565173035aeee67847

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,16 @@
 class Pirate
+  @@all = []
+
+  def self.all
+    @@all
+  end
+
+  attr_reader :name, :height, :weight
+
+  def initialize(name, height, weight)
+    @name = name
+    @height = height
+    @weight = weight
+    @@all << self
+  end
 end
